b9f8a774a798e1bf3d7935b3d698ebbfb4507bc2
Diego Palacio <palaciodiego099@gmail.com>
2023-02-07 10:41:24 -0500

    Added cors

--- pkg/tasks/controller.go
+++ pkg/tasks/controller.go
@@ -2,6 +2,7 @@ package tasks
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"gorm.io/gorm"
 )
 
@@ -11,6 +12,19 @@ type handler struct {
 
 func Routers(app *fiber.App, db *gorm.DB) {
 	h := handler{DB: db}
+
+	//using cors
+	app.Use(cors.New())
+
+	app.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders:     "",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+	}))
 
 	var taskGroup = app.Group("/tasks")
 	taskGroup.Get("/", h.List)
